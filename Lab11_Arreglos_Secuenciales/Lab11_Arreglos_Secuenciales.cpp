75d49f5f581fd9afda96c6a87d644070e68fc4ca
Garie <Garie@COMPUTADORA3>
2025-04-21 20:10:14 -0600

    Agregar archivos de proyecto.

---
+++ Lab11_Arreglos_Secuenciales/Lab11_Arreglos_Secuenciales.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numeros[10];
+
+    for (int i = 0; i < 10; i++) {
+        numeros[i] = i + 1;
+    }
+
+
+    for (int i = 0; i < 10; i++) {
+        cout << "Índice " << i << ": " << numeros[i] << endl;
+    }
+
+    return 0;
+}
